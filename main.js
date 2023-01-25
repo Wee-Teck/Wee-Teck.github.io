cbb7f037a2702b0c0a5b74459c91a4519f9f2e9e
WeeTeck <lee.weetecx@gmail.com>
2023-01-25 15:29:34 +0800

    improved error handling and UI using bootstrap

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
 import { EquationSolver } from "./EquationSolver.js";
-import {Result } from "./Result.js";
+import { Result } from "./Result.js";
 
 window.onload = function () {
   new EquationSolverFactory();
 };
 
-function EquationSolverFactory() {
-  this.input = document.getElementById(EquationSolverFactory.inputId);
-  this.output = document.getElementById(EquationSolverFactory.outputId);
-  this.submitBtn = document.getElementById(EquationSolverFactory.submitBtnId);
+class EquationSolverFactory {
+  constructor() {
+    this.input = document.getElementById(EquationSolverFactory.inputId);
+    this.output = document.getElementById(EquationSolverFactory.outputId);
+    this.submitBtn = document.getElementById(EquationSolverFactory.submitBtnId);
 
-  this.input.placeholder = "3 ^ (4 - 2) + 1";
+    this.input.placeholder = "3 ^ (4 - 2) + 1";
 
-  var $this = this;
-  this.submitBtn.addEventListener("click", function (e) {
-    $this.submitBtnDidClick(e);
-  });
-}
-
-EquationSolverFactory.prototype.submitBtnDidClick = function (e) {
-  var eqn = this.input.value.replace(/\s/g, "");
-  var $this = this;
-  var eqs = new EquationSolver(eqn);
-  console.log(eqs);
-  var result = this.createOutput(eqn, eqs.result, eqs.error);
-  this.output.insertBefore(result.html, this.output.firstChild);
-  result.html.addEventListener("click", function (e) {
-    $this.resultDidClick(result);
-  });
-  if (this.output.children.length > 10) {
-    this.output.removeChild(this.output.lastChild);
+    var $this = this;
+    this.submitBtn.addEventListener("click", function (e) {
+      $this.submitBtnDidClick(e);
+    });
   }
-};
 
-EquationSolverFactory.prototype.resultDidClick = function (result) {
-  this.input.value = result.input;
-};
+  submitBtnDidClick(e) {
+    var eqn = this.input.value.replace(/\s/g, "");
+    var $this = this;
+    var eqs = new EquationSolver(eqn);
+    console.log("eqs", eqs);
+    var result = this.createOutput(eqn, eqs.result, eqs.error);
+    this.output.insertBefore(result.html, this.output.firstChild);
+    result.html.addEventListener("click", function (e) {
+      $this.resultDidClick(result);
+    });
+    if (this.output.children.length > 10) {
+      this.output.removeChild(this.output.lastChild);
+    }
+  }
 
-EquationSolverFactory.prototype.createOutput = function (eqn, soln, isError) {
-  return new Result(eqn, soln, isError);
-};
+  resultDidClick(result) {
+    this.input.value = result.input;
+  }
+
+  createOutput(eqn, soln, isError) {
+    return new Result(eqn, soln, isError);
+  }
+}
 
 EquationSolverFactory.inputId = "equation_input";
 EquationSolverFactory.outputId = "equation_output";
